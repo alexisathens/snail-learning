ebca2c55cf0e748611f834b4eeb1f9a67d050cd7
lexisathens1 <lexisathens1@gmail.com>
2018-12-09 12:33:14 -0500

    machine learning methods

--- script.R
+++ script.R
@@ -9,8 +9,11 @@ library(sp) #spatial stuff
 library(mgcv) #GAMs
 library(randomForest) #random forests
 library(geosphere) #distm function
-library(ggplot2)
+library(ggplot2) #plot graphs
 library(leaps) #forward selection
+library(splines) #ns function in GAM
+library(glmnet) #lasso
+library(pROC) #ROC curves
 
 #========================= Data Manipulation =============================
 #import and clean data
@@ -23,11 +26,9 @@ full <- full[,1:19]
 #remove faulty observations
 full <- full[-which(full$State=="PR"),] #remove 2 observations from Puerto Rico
 full <- full[-which(full$State=="AZ"),] #remove 1 observation from Arizona
-#full <- full[-which(full$State=="AZ"),] #remove x observations from Texas (read lit)
 
 #subset into coordinates
 snail <- full[,c("Longitude","Latitude")]
-#colnames(snail)
 
 #========
 #download bioclim data
@@ -100,9 +101,10 @@ y <- c(rep(1, nrow(presence)), rep(0, nrow(absence))) #binary response
 y <- factor(y) #binary classification
 
 
-#========================= Machine Learning Methods =============================
+#========================= Spatial Blocking =============================
 
 #===== classify observations into blocks
+set.seed(0)
 coords <- dat[,1:2] #coordinates of all points
 
 #define many blocks
@@ -137,7 +139,7 @@ for(i in 1:nrow(coords)){
 
 #plot the classifications
 gg <- ggplot(coords, aes(x=Longitude,y=Latitude,label=Block)) + 
-  geom_text(check_overlap = TRUE)
+  geom_text(check_overlap = TRUE) + ggtitle("Spatially blocked observations")
 gg
 
 #count number of points in each group
@@ -147,15 +149,15 @@ for(i in 1:nrow(centroids)){
 }
 
 #stats on the blocks
-nblocks #111 blocks defined
-mean(centroids$count) #mean of 13
-median(centroids$count) #median of 10
+nblocks #116 blocks defined
+mean(centroids$count) #mean of 12.5
+median(centroids$count) #median of 9
 min(centroids$count) #min 3 observations
-max(centroids$count) #max 66 observations
+max(centroids$count) #max 48 observations
 
 
 #====== classify observations into folds
-#set.seed(0)
+set.seed(0)
 k <- 10 #k=10 folds
 
 #divide blocks into k folds (from p.250 lab)
@@ -170,83 +172,246 @@ for(i in 1:nrow(coords)){
 folds.obs <- coords$Fold #each observation classified into single fold
 
 
-#====== forward stepwise selection
+#========================= Machine Learning Methods =============================
+
+#===== simple logistic regression
+set.seed(0)
+val.errors <- rep(NA, k)
+pred.vals <- actual.vals <- numeric()
+
+for(i in 1:k){
+  glm.fit <- glm(y[folds.obs!=i]~., family="binomial", dat=dat[folds.obs!=i,3:19]) #all but coords
+  pred <- predict(glm.fit, newdata=dat[folds.obs==i,3:19], type="response") #group membership probs
+  pred <- ifelse(pred>0.5,1,0) #classify to presence (1) if probability > 0.5
+  val.errors[i] <- sum(I(pred != y[folds.obs==i]))/length(pred) #prob of misclassifying
+  
+  #for roc curve
+  pred.vals <- c(pred.vals, pred)
+  actual.vals <- c(actual.vals, y[folds.obs==i])
+}
+glm.misclass <- mean(val.errors)
+glm.misclass #misclassified observations 12% of time
+
+glm.roc <- roc(as.numeric(pred.vals), as.numeric(actual.vals))
+plot(glm.roc)
+glm.roc$auc #0.8713
+
+
+#===== forward feature selection
+set.seed(0)
 val.errors <- matrix(data=NA,nrow=ncol(dat),ncol=k)
+
 for(i in 1:k){
   fit.fwd <- regsubsets(y[folds.obs!=i]~., data=dat[folds.obs!=i,], nvmax=ncol(dat),
-                        method="forward") #get subsets for ith fold
-  test.mat <- model.matrix(y[folds.obs!=i]~., data=dat[folds.obs!=i,])
+                        method="forward") #order best predictors for ith fold
   for(j in 1:ncol(dat)){
-    coefi <- coef(fit.fwd,id=j)
-    reg <- test.mat[,names(coefi)] %*% coefi #simple regression equation
-    pred <- exp(reg)/(1+exp(reg)) #predict probabilities of group membership
-    pred <- ifelse(pred>0.5,1,0) #classify to group 1 if probability > 0.5
-    val.errors[j,i] <- sum(I(pred != y[folds.obs!=i]))/length(pred) #probability of misclassifying
+    coefi <- coef(fit.fwd, id=j) #get names of best j predictors
+    best.pred <- names(coefi)[-1] #get rid of intercept
+    #write new formula
+    f <- as.formula(paste("y[folds.obs!=i] ~", paste(best.pred, collapse = " + ")))
+    #use glm to predict class
+    glm.fit <- glm(f, family="binomial", dat=dat[folds.obs!=i,])
+    pred <- predict(glm.fit, newdata=dat[folds.obs==i,], type="response") #get probs
+    pred <- ifelse(pred>0.5,1,0) #classify to presence (1) if probability > 0.5
+    val.errors[j,i] <- sum(I(pred != y[folds.obs==i]))/length(pred) #prob of misclassifying
   }
 }
+avg.val.errors <- apply(val.errors,1,mean) #take average performance across k folds (cols)
+avg.val.errors
 
-val.errors
-#take average across columns
-#then find min row -> optimal set of predictors
+opt.pred <- base::which.min(avg.val.errors) #select set of predictors with lowest val error
+opt.pred #optimal to use 14/19 predictors
+avg.val.errors[opt.pred] #misclassification rate of 11.11%
 
 
-
-
-
-#======forward stepwise selection without CV
-fit.fwd <- regsubsets(y~., data=dat.transformed, nvmax=ncol(dat.transformed),
-                      method="forward")
+#====== get best predictors
+fit.fwd <- regsubsets(y~., data=dat, nvmax=ncol(dat), method="forward")
 fwd.sum <- summary(fit.fwd)
-names(fwd.sum) #see types of decision criteria
-fwd.sum$rsq # Rsq increases from 51.3% with one var to 60.7% with all vars included
-
-#check RSS and adj-R2 stats
-plot(fwd.sum$rss, xlab="Number of vars", ylab="RSS", type="l")
-plot(fwd.sum$adjr2, xlab="Number of vars", ylab="Adj-Rsq", type="l")
-n.fwd <- which.max(fwd.sum$adjr2) #optimal number of predictors to include is 17/19
-points(n.fwd, fwd.sum$adjr2[n.fwd], col="red", cex=2, pch=20)
-
-#do the same for Cp and BIC stats
-plot(fwd.sum$cp, xlab="Number of vars", ylab="Cp", type="l")
-which.min(fwd.sum$cp) #min at 16
-plot(fwd.sum$bic, xlab="Number of vars", ylab="BIC", type="l")
-which.min(fwd.sum$bic) #min at 8!
-
-#plot all
-plot(fit.fwd, scale="r2")
-
-coef(fit.fwd, 8)
+fwd.sum$rsq # Rsq increases from 48.9% with one var to 58.7% with 17 best/all vars included
+#plot rsq
+plot(fwd.sum$rsq, xlab="Number of vars", ylab="Rsq", type="l")
+points(opt.pred, fwd.sum$rsq[opt.pred], col="red", cex=2, pch=20)
 
 
+#====== lasso shrinkage method
+set.seed(0)
+val.errors <- rep(NA, k)
+pred.vals <- actual.vals <- numeric()
+grid=10^seq(10,-2,length=100) #lambda values for lasso
 
-#===== non-spatial logistic regression
-#transform predictors
-dat.transformed <- base::scale(dat, center=T, scale=T)
-dat.transformed <- as.data.frame(dat.transformed)
+for(i in 1:k){
+  lasso.fit <- glmnet(x=as.matrix(dat[folds.obs!=i,]),y=as.matrix(y[folds.obs!=i]), alpha=1,
+                      lambda=grid) #calculate with many tuning parameters
+  min.lambda <- min(lasso.fit$lambda)
+  pred <- predict(lasso.full, s=min.lambda, newx=as.matrix(dat[folds.obs==i,]))
+  pred <- ifelse(pred>0.5,1,0) #classify to presence (1) if probability > 0.5
+  val.errors[i] <- sum(I(pred != y[folds.obs==i]))/length(pred) #prob of misclassifying
+  
+  #for roc curve
+  pred.vals <- c(pred.vals, pred)
+  actual.vals <- c(actual.vals, y[folds.obs==i])
+}
+lasso.misclass <- mean(val.errors) 
+lasso.misclass #misclassified observations 12.3% of time
 
-#fit logistic regression model
-glm.coords <- glm(y~Longitude+Latitude, family="binomial", dat=dat.transformed) #coords only
-glm.nocoords <- glm(y~., family="binomial", dat=dat.transformed[,3:19]) #all but coords
-glm.few <- glm(y~bio5+bio14+bio15, family="binomial", dat=dat.transformed) #few good predictors
+plot(lasso.fit) #see effect of different tuning parameters
 
-AIC(glm.few,glm.coords,glm.nocoords) #glm.nocoords has lowest AIC
-summary(glm.nocoords)
-
-#use train here instead
-
-#use raster package to predict
-glm.raster <- raster::predict(model=glm.nocoords, object=bioclim.dat, fun=predict)
-glm.raster <- exp(glm.raster) / (1 + exp(glm.raster))
-plot(glm.raster, xlab = "Longitude", ylab = "Latitude")
+lasso.roc <- roc(as.numeric(pred.vals), as.numeric(actual.vals))
+plot(lasso.roc)
+lasso.roc$auc #0.8689
 
 
-#GAM
-#use CV for optimal # of knots
-gam.nocoords <- gam(y~bio1+bio2+bio4+bio5+bio6+bio8+bio9+bio10+bio11+bio12+bio13+
-                      bio14+bio15+bio16+bio17+bio18+bio19, 
-                    family="binomial", data=dat.transformed) #all but coords
+#===== GAM
+set.seed(0)
+
+#==natural splines for single best predictor
+val.errors <- rep(NA, k)
+pred.vals <- actual.vals <- numeric()
+
+for(i in 1:k){
+  #fit natural splines for single best predictors
+  gam.fit.1 <- gam(y[folds.obs!=i]~bio1+bio2+bio4+bio5+bio6+bio8+bio9+bio10+bio11+bio12+
+                     bio13+bio14+bio15+bio16+bio17+ns(bio18,5)+bio19, family="binomial", 
+                   data=dat[folds.obs!=i,])
+  pred <- predict(gam.fit.1, newdata=dat[folds.obs==i,3:19], type="response") #group membership probs
+  pred <- ifelse(pred>0.5,1,0) #classify to presence (1) if probability > 0.5
+  val.errors[i] <- sum(I(pred != y[folds.obs==i]))/length(pred) #prob of misclassifying
+  
+  #for roc curve
+  pred.vals <- c(pred.vals, pred)
+  actual.vals <- c(actual.vals, y[folds.obs==i])
+}
+gam.misclass.1 <- mean(val.errors) 
+gam.misclass.1 #misclassified observations 12% of time
+
+gam.roc.1 <- roc(as.numeric(pred.vals), as.numeric(actual.vals))
+plot(gam.roc.1)
+gam.roc.1$auc #0.8812
+
+
+#==natural splines for best 3 predictors
+val.errors <- rep(NA, k)
+pred.vals <- actual.vals <- numeric()
+
+for(i in 1:k){
+  #fit natural splines for 3 best predictors
+  gam.fit.3 <- gam(y[folds.obs!=i]~bio1+bio2+bio4+bio5+bio6+ns(bio8,5)+bio9+bio10+bio11+bio12+
+                     ns(bio13,5)+bio14+bio15+bio16+bio17+ns(bio18,5)+bio19, family="binomial", 
+                   data=dat[folds.obs!=i,])
+  pred <- predict(gam.fit.3, newdata=dat[folds.obs==i,3:19], type="response") #group membership probs
+  pred <- ifelse(pred>0.5,1,0) #classify to presence (1) if probability > 0.5
+  val.errors[i] <- sum(I(pred != y[folds.obs==i]))/length(pred) #prob of misclassifying
+  
+  #for roc curve
+  pred.vals <- c(pred.vals, pred)
+  actual.vals <- c(actual.vals, y[folds.obs==i])
+}
+gam.misclass.3 <- mean(val.errors) 
+gam.misclass.3 #misclassified observations 12% of time
+
+gam.roc.3 <- roc(as.numeric(pred.vals), as.numeric(actual.vals))
+plot(gam.roc.3)
+gam.roc.3$auc #0.8812
+
+
+#==natural splines for best 5 predictors
+val.errors <- rep(NA, k)
+pred.vals <- actual.vals <- numeric()
+
+for(i in 1:k){
+  #fit natural splines for 5 best predictors
+  gam.fit.5 <- gam(y[folds.obs!=i]~ns(bio1,5)+ns(bio2,5)+bio4+bio5+bio6+ns(bio8,5)+bio9+bio10+
+                     bio11+bio12+ns(bio13,5)+bio14+bio15+bio16+bio17+ns(bio18,5)+bio19, 
+                   family="binomial", data=dat[folds.obs!=i,])
+  pred <- predict(gam.fit.5, newdata=dat[folds.obs==i,3:19], type="response") #group membership probs
+  pred <- ifelse(pred>0.5,1,0) #classify to presence (1) if probability > 0.5
+  val.errors[i] <- sum(I(pred != y[folds.obs==i]))/length(pred) #prob of misclassifying
+  
+  #for roc curve
+  pred.vals <- c(pred.vals, pred)
+  actual.vals <- c(actual.vals, y[folds.obs==i])
+}
+gam.misclass.5 <- mean(val.errors) 
+gam.misclass.5 #misclassified observations 13% of time
+
+gam.roc.5 <- roc(as.numeric(pred.vals), as.numeric(actual.vals))
+plot(gam.roc.5)
+gam.roc.5$auc #0.8812
+
+
+#==natural splines for best 8 predictors
+val.errors <- rep(NA, k)
+pred.vals <- actual.vals <- numeric()
+
+for(i in 1:k){
+  #fit natural splines for 8 best predictors
+  gam.fit.8 <- gam(y[folds.obs!=i]~ns(bio1,5)+ns(bio2,5)+bio4+ns(bio5,5)+ns(bio6,5)+ns(bio8,5)+
+                     bio9+bio10+ns(bio11,5)+bio12+ns(bio13,5)+bio14+bio15+bio16+bio17+ns(bio18,5)+
+                     bio19, family="binomial", data=dat[folds.obs!=i,])
+  pred <- predict(gam.fit.8, newdata=dat[folds.obs==i,3:19], type="response") #group membership probs
+  pred <- ifelse(pred>0.5,1,0) #classify to presence (1) if probability > 0.5
+  val.errors[i] <- sum(I(pred != y[folds.obs==i]))/length(pred) #prob of misclassifying
+  
+  #for roc curve
+  pred.vals <- c(pred.vals, pred)
+  actual.vals <- c(actual.vals, y[folds.obs==i])
+}
+gam.misclass.8 <- mean(val.errors) 
+gam.misclass.8 #misclassified observations 13.5% of time
+
+gam.roc.8 <- roc(as.numeric(pred.vals), as.numeric(actual.vals))
+plot(gam.roc.8)
+gam.roc.8$auc #0.8469
 
 
 #====== random forest
-#model <- y ~ bio1 + bio2 + bio3 + bio4 + bio5 + bio6 + bio7 + bio8 + bio9 + bio10 + bio11 +
-#  bio12 + bio13 + bio14 + bio15 + bio16 + bio17
+set.seed(0)
+val.errors <- rep(NA, k)
+pred.vals <- actual.vals <- numeric()
+
+for(i in 1:k){
+  forest.fit <- randomForest(y[folds.obs!=i]~bio1+bio2+bio4+bio5+bio6+bio8+bio9+bio10+bio11+
+                               bio12+bio13+bio14+bio15+bio16+bio17+bio18+bio19,
+                             data=dat[folds.obs!=i,3:19]) #all but coords
+  pred <- predict(forest.fit, newdata=dat[folds.obs==i,3:19], type="response") #predict category
+  val.errors[i] <- sum(I(pred != y[folds.obs==i]))/length(pred) #prob of misclassifying
+  
+  #for roc curve
+  pred.vals <- c(pred.vals, pred)
+  actual.vals <- c(actual.vals, y[folds.obs==i])
+}
+forest.misclass <- mean(val.errors)
+forest.misclass #misclassified observations 12% of time
+
+forest.roc <- roc(as.numeric(pred.vals), as.numeric(actual.vals))
+plot(forest.roc)
+forest.roc$auc #0.8362
+
+
+#====== plot ROC curves
+plot(glm.roc, col = 1, lty = 2, main = "ROC")
+plot(gam.roc.3, col = 2, lty = 3, add = TRUE)
+plot(gam.roc.5, col = 3, lty = 3, add = TRUE)
+plot(gam.roc.8, col = 4, lty = 3, add = TRUE)
+plot(forest.roc, col = 5, lty = 3, add = TRUE)
+plot(lasso.roc, col = 6, lty = 3, add = TRUE)
+legend(0.4, 0.7, legend=c("GLM", "GAM 3 NS", "GAM 5 NS", "GAM 8 NS", "R.  Forest", "Lasso"),
+       col=c(1,2,3,4,5,6), lty=c(2,3,3,3,3,3), cex=0.8, box.lty=0)
+
+
+#====== use raster package to predict and plot snail distribution
+glm.raster <- raster::predict(model=glm.fit, object=bioclim.dat, fun=predict)
+glm.raster <- exp(glm.raster) / (1 + exp(glm.raster))
+plot(glm.raster, xlab = "Longitude", ylab = "Latitude", 
+     main="Projected distribution of apple snail")
+
+#====== plot best predictors
+plot(bioclim.dat[[16]], main="Precipitation of warmest quarter") #plot bio18
+points(snail$Longitude, snail$Latitude, col='blue', pch=20, cex=0.75) #add observations
+
+plot(bioclim.dat[[6]], main="Mean temp of wettest quarter") #plot bio8
+points(snail$Longitude, snail$Latitude, col='blue', pch=20, cex=0.75) #add observations
+
+plot(bioclim.dat[[11]], main="Precipitation of wettest month") #plot bio13
+points(snail$Longitude, snail$Latitude, col='blue', pch=20, cex=0.75) #add observations
+
