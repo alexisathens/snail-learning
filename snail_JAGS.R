0273604df00123d31e541ca2c1c9df36e99912c8
Alexis Athens <lexisathens1@gmail.com>
2019-04-19 12:44:25 -0400

    return probabilities

--- snail_JAGS.R
+++ snail_JAGS.R
@@ -1,21 +1,17 @@
 model{
   for (i in 1:nobs){
-    #eta[i] <- b5*x1[i] + b12*x2[i] + b14*x3[i] + b15*x4[i] + b16*x5[i] + b18*x6[i] + b19*x7[i]
-    #pi[i] <- 1/(1+exp(-eta[i]))
-    #logit(pi[i]) <- -(b0+b5*x1[i] + b12*x2[i] + b14*x3[i] + b15*x4[i] + b16*x5[i] + 
-                              #b18*x6[i] + b19*x7[i])
-    y[i] ~ dbern(logit(-(b0+b5*x1[i] )))
-    #+ b12*x2[i] + b14*x3[i] + b15*x4[i] + b16*x5[i] + b18*x6[i] + b19*x7[i]
+    eta[i] <- b5*x1[i] + b12*x2[i] + b14*x3[i] + b15*x4[i] + b16*x5[i] + b18*x6[i] + b19*x7[i]
+    pi[i] <- 1/(1+exp(-eta[i]))
   }
   
   #priors
   b0~dnorm(0,.01)
   b5~dnorm(0,.01)
-  #b12~dnorm(0,.01)
-  #b14~dnorm(0,.01)
-  #b15~dnorm(0,.01)
-  #b16~dnorm(0,.01)
-  #b18~dnorm(0,.01)
-  #b19~dnorm(0,.01)
+  b12~dnorm(0,.01)
+  b14~dnorm(0,.01)
+  b15~dnorm(0,.01)
+  b16~dnorm(0,.01)
+  b18~dnorm(0,.01)
+  b19~dnorm(0,.01)
 }
 
